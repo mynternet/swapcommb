a7cb0e48f89c2cace60636be83c6fc4847418b8a
mynternet <mynternet@gmail.com>
2023-12-09 07:55:39 -0500

    first commit

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
 const { typeDefs, resolvers } = require("./schemas");
@@ -6,24 +7,37 @@ const db = require("./config/connection");
 const { authMiddleware } = require("./utils/auth");
 const routes = require("./routes");
 
+// Initialize Express
 const app = express();
+
+// CORS configuration
+const corsOptions = {
+  origin: "https://swapcomm.vercel.app", // Your frontend URL
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204
+};
+
+// Enable CORS using the corsOptions
+app.use(cors(corsOptions));
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Initialize Apollo Server for GraphQL
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware
 });
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// Use routes
-app.use(routes);
-
 // Connect ApolloServer with Express
 const startApolloServer = async () => {
   await server.start();
   server.applyMiddleware({ app });
 
+  // Database connection
   db.once("open", () => {
     console.log(`Connected to database`);
   });
@@ -31,5 +45,9 @@ const startApolloServer = async () => {
 
 startApolloServer();
 
+// Use routes (for additional REST API endpoints if you have any)
+app.use(routes);
+
+// Export the app for serverless deployment
 module.exports = app;
 
