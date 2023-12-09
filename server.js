11c6c201aca1db4fca65f058dbe7da1ba70f1c5e
mynternet <mynternet@gmail.com>
2023-12-09 11:22:04 -0500

    first commit

--- server.js
+++ server.js
@@ -1,33 +1,43 @@
 const express = require("express");
+const cors = require("cors");
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 const routes = require("./routes");
-
-//const path = require("path");
 const db = require("./config/connection");
-//const { Console } = require("console");
 
 // Initialize Express app
 const app = express();
 
-// Initialize Apollo Server for GraphQL
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-  introspection: true, // Enable introspection (necessary for GraphQL Playground)
-  playground: true,  // Enable GraphQL Playground
-});
+// CORS configuration
+const corsOptions = {
+    //origin: "https://swapcomm.vercel.app", // Replace with your frontend domain
+    origin: '*', 
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // Allow session cookie from browser to pass through
+    optionsSuccessStatus: 204
+};
+
+// Enable CORS using the corsOptions
+app.use(cors(corsOptions));
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Initialize Apollo Server for GraphQL
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+    introspection: true, // Enable introspection (necessary for GraphQL Playground)
+    playground: true,  // Enable GraphQL Playground
+});
+
 // Connect ApolloServer with Express
 const startApolloServer = async () => {
-  await server.start();
-  server.applyMiddleware({ app });
+    await server.start();
+    server.applyMiddleware({ app });
 };
 
 startApolloServer();
@@ -35,10 +45,8 @@ startApolloServer();
 // Use routes for additional REST API endpoints
 app.use(routes);
 
-module.exports = app;
-
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
 
