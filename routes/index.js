5cefe460c1dc8c3ec5e6771a98b54016c9d17fcf
mynternet <mynternet@gmail.com>
2023-12-09 07:25:58 -0500

    backend

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const path = require("path");
 
 router.use((req, res) => {
-  res.sendFile(path.join(__dirname, "../../frontend/build/index.html"));
+  res.sendFile(path.join(__dirname, "https://swapcomm.vercel.app"));
 });
 
 module.exports = router;
